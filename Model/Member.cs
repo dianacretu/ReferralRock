9e275be7daad5ad7cb8a447211c72b4bc983662e
Diana Cretu <dianacretu2806@gmail.com>
2023-12-29 16:38:27 +0200

    Add get members

---
+++ Model/Member.cs
@@ -0,0 +1,29 @@
+﻿namespace ReferralRock.Model
+{
+    public class MemberApiResponse<Member>
+    {
+        public int Offset { get; set; }
+        public int Total { get; set; }
+        public string Message { get; set; }
+        public List<Member> Members { get; set; }
+    }
+
+    public class Member
+    {
+        public string Id { get; set; }
+        public string DisplayName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string ExternalIdentifier { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string AddressLine1 { get; set; }
+        public string AddressLine2 { get; set; }
+        public string City { get; set; }
+        public string CountrySubdivision { get; set; }
+        public string Country { get; set; }
+        public string PostalCode { get; set; }
+        public bool DisabledFlag { get; set; }
+    }
+}
---
+++ Pages/PageModelWithHttpClient.cshtml.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using ReferralRock.Model;
+using System.Text.Json;
+
+
+    public class PageModelWithHttpClient : PageModel
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        private readonly IConfiguration _configuration;
+
+        public PageModelWithHttpClient(IHttpClientFactory httpClientFactory, IConfiguration configuration)
+        {
+            _httpClientFactory = httpClientFactory;
+            _configuration = configuration;
+        }
+
+        private HttpClient CreateClient()
+        {
+            string apiKey = _configuration["AppSettings:ApiKey"];
+            var httpClient = _httpClientFactory.CreateClient("client");
+
+            httpClient.DefaultRequestHeaders.Add("Authorization", "Basic " + apiKey);
+            return httpClient;
+        }
+
+        public async Task<List<Member>> GetMembers()
+        {
+            var parameters = new Dictionary<string, string>
+            {
+                { "programId", _configuration["AppSettings:ProgramId"] }
+            };
+
+            var queryString = string.Join("&", parameters.Select(p => $"{Uri.EscapeDataString(p.Key)}={Uri.EscapeDataString(p.Value)}"));
+            var apiUrl = $"api/members?{queryString}";
+
+            using (var httpClient = CreateClient())
+            {
+                var response = await httpClient.GetAsync(apiUrl);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var content = await response.Content.ReadAsStringAsync();
+                    MemberApiResponse<Member> apiResponse = JsonSerializer.Deserialize<MemberApiResponse<Member>>(content, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+
+                    return apiResponse.Members;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
+    }
+
